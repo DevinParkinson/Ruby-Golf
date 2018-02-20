a4dc7a00a3efbc9f8375b9b3e43da57edc9347e8
Devin Parkinson <dparky13@icloud.com>
2018-02-19 18:18:24 -0700

    Still working

--- rubygolf.rb
+++
@@ -1,7 +0,0 @@
-a = [1, 2, 3]
-
-def sum_arr(a)
-  a.reduce(&:+)
-end
-
-puts sum_arr
--- rubygolf7-9.rb
+++ rubygolf7-9.rb
@@ -1,9 +1,27 @@
 # MIXED PAIRS
 
-def mixed(a)
-  c = a.length
-  x = a.flatten.shuffle
-  m = %(a..z)
-  c.times { m << x.pop(2) }
-  m
+# def mix(x)
+#   a = y.length
+#   x = y.flatten.shuffle
+#   b = %(a..z)
+#   a.times { b << x.pop(2) }
+#   b
+# end
+# 73
+
+# LOVE TEST
+@a = []
+@b = []
+@t = 0
+def love_test(a, b)
+  puts "Who is the first name?"
+  @a = gets
+  puts "Who is the second?"
+  @b = gets
+  @t = 0
+  l = @a.length + @b.length
+  @a.split('').each { |c| @b.split('').include?(c) && @t += 1 }
+  l / @t
+  p @ t
 end
+love_test(@a, @b)
